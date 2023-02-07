f89380665722c2dc51a6442c6e74a35c90efb168
Peter Dodge <ptr.dodge@gmail.com>
2023-02-07 12:54:02 -0800

    readme match code

--- Database.php
+++ Database.php
@@ -2,38 +2,51 @@
 
 class Database
 {
+    // define public variables that can be changed
     public string $host = "localhost";
     public string $username = "root";
     public string $password = "";
     public string $database_name = "";
     public string $table_name = "";
     public bool $verbose = false;
+    public bool $use_echo_for_errors = false;
     public $connection;
 
     function __construct($host, $username, $password, $database_name, $table_name)
     {
-
+        // take in arguments and set them to the public variables
         $this->host = $host;
         $this->username = $username;
         $this->password = $password;
         $this->database_name = $database_name;
         $this->table_name = $table_name;
 
+        // connect to mySQL server
         $this->connect();
 
-        // If database doesnt exist, create it
+        // Check if the given database doesnt exist
         if (!$this->database_exists($this->database_name)) {
+            // if it doesnt exist, create it
             $this->run_sql("CREATE DATABASE $this->database_name");
+
+            // reset connection to include the database
+            $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database_name);
         } else {
-            // database is already created
+            // database is already created, connect to it
             $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database_name);
         }
     }
 
-    function console_log($error_message)
+    private function console_log($error_message)
     {
+        // just a convenience function, $this->verbose is false by default
         if ($this->verbose) {
-            echo '<script>console.log("' . $error_message . '")</script>';
+            // if you cant understand an if statement, im not explaining
+            if ($this->use_echo_for_errors) {
+                echo $error_message;
+            } else {
+                echo '<script>console.log("' . $error_message . '")</script>';
+            }
         }
     }
 
@@ -45,8 +58,7 @@ class Database
 
     function check_connection()
     {
-        $connection = $this->connection;
-        if ($connection->connect_error) {
+        if ($this->connection->connect_error) {
             $this->console_log("Connection failed");
         } else {
             $this->console_log("Connected to mysql");
@@ -55,21 +67,22 @@ class Database
 
     function run_sql(string $sql)
     {
-        $connection = $this->connection;
-        if ($connection->query($sql) === TRUE) {
+        if ($this->connection->query($sql) === TRUE) {
             $this->console_log("SQL ran successfully");
         } else {
             $this->console_log("Error running SQL");
-            echo $connection->error;
         }
     }
 
     function database_exists($db_name)
     {
-        $connection = $this->connection;
-        $result = $connection->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db_name'");
+        if (!isset($db_name)) {
+            $db_name = $this->database_name;
+        }
 
-        if ($result->num_rows) {
+        $check_for_database = $this->connection->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db_name'");
+
+        if ($check_for_database->num_rows) {
             $this->console_log("Database exists");
             return true;
         } else {
@@ -80,8 +93,15 @@ class Database
 
     function table_exists($database, $table)
     {
-        $connection = $this->connection;
-        $table_exists = $connection->query("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$database' AND TABLE_NAME = '$table'");
+        if (!isset($database)) {
+            $database = $this->database_name;
+        }
+
+        if (!isset($table)) {
+            $table = $this->table_name;
+        }
+
+        $table_exists = $this->connection->query("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$database' AND TABLE_NAME = '$table'");
 
         if ($table_exists->num_rows) {
             $this->console_log("Table exists");
--- readme.md
+++ readme.md
@@ -1,9 +1,8 @@
-# Database.php
+# Database
 
-really cliche name for something any php user would use
+> really cliche name for something any php user would use
 
-user like this:
-
+## Create an instance
 ```php
 $host = "localhost";
 $username = "root";
@@ -11,4 +10,34 @@ $password = "";
 $database_name = "";
 
 $db = new Database($host, $username, $password, $database_name);  
+
 ```
+
+## Make queries
+```php
+$query = "SELECT * FROM $some_table WHERE $some_row = $some_col";
+$db->run_sql($query);
+```
+
+## Check the connection at any point
+```php
+$some_db = "database1";
+$db->check_connection($some_db);
+// uses $db->database_name by default
+```
+
+## Check existence of a database
+```php
+$some_db = "database1";
+$db->database_exists($some_db);
+// uses $db->database_name by default
+```
+
+## Check existence of a table
+```php
+$some_db = "table1";
+$db->table_exists($some_db);
+// uses $db->table_name by default
+```
+
+thats all for now
